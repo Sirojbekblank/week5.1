9c2eee26fd9c30ddd3747e907469007d80eda9c3
Sirojbekblank <sirojbekblank@gmail.com>
2024-09-29 19:40:26 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -10,27 +10,22 @@ int main() {
     int position =0;
     for (int i = 1; true; i++) {
         bool result = true;
-        bool final;
         int original =i;
-            while (i>0){
+            while (original>0){
 
-                if(i%10==3) {
+                if(original%10==3) {
                     result = false;
                     break;
                 }
-                i/=10;
+                original/=10;
             }
 
-        i= original;
         if(result==true && i%3!=0) {
             position++;
             if(position==a) {
-                final=true;
                 cout<<i<<endl;
+                break;
             }
-        }
-        if(final==true) {
-            break;
         }
 
     }
